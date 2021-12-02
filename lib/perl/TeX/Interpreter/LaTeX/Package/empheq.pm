c8890ec635ad19b053a33151516eaa9389c0dd33
David M. Jones <dmj@ams.org>
2021-12-02 18:27:22 -0500

    TeX::Interpreter::LaTeX::Package::empheq, v1.1.0: Added remaining macros from MathJax v3 empheq extension.

--- lib/perl/TeX/Interpreter/LaTeX/Package/empheq.pm
+++ lib/perl/TeX/Interpreter/LaTeX/Package/empheq.pm
@@ -3,7 +3,7 @@ package TeX::Interpreter::LaTeX::Package::empheq;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv '1.0.0';
+use version; our $VERSION = qv '1.1.0';
 
 sub install ( $ ) {
     my $class = shift;
@@ -30,7 +30,42 @@ __DATA__
 
 \DefineAMSTaggedEnvironment{empheq}\st@rredfalse
 
+\DeclareMathPassThrough{empheqbiglVert}
+\DeclareMathPassThrough{empheqbiglangle}
+\DeclareMathPassThrough{empheqbiglbrace}
+\DeclareMathPassThrough{empheqbiglbrack}
+\DeclareMathPassThrough{empheqbiglceil}
+\DeclareMathPassThrough{empheqbiglfloor}
+\DeclareMathPassThrough{empheqbiglparen}
+\DeclareMathPassThrough{empheqbiglvert}
+\DeclareMathPassThrough{empheqbigl}
+\DeclareMathPassThrough{empheqbigrVert}
+\DeclareMathPassThrough{empheqbigrangle}
+\DeclareMathPassThrough{empheqbigrbrace}
+\DeclareMathPassThrough{empheqbigrbrack}
+\DeclareMathPassThrough{empheqbigrceil}
+\DeclareMathPassThrough{empheqbigrfloor}
+\DeclareMathPassThrough{empheqbigrparen}
+\DeclareMathPassThrough{empheqbigrvert}
+\DeclareMathPassThrough{empheqbigr}
+\DeclareMathPassThrough{empheqlVert}
+\DeclareMathPassThrough{empheqlangle}
 \DeclareMathPassThrough{empheqlbrace}
+\DeclareMathPassThrough{empheqlbrack}
+\DeclareMathPassThrough{empheqlceil}
+\DeclareMathPassThrough{empheqlfloor}
+\DeclareMathPassThrough{empheqlparen}
+\DeclareMathPassThrough{empheqlvert}
+\DeclareMathPassThrough{empheql}
+\DeclareMathPassThrough{empheqrVert}
+\DeclareMathPassThrough{empheqrangle}
+\DeclareMathPassThrough{empheqrbrace}
+\DeclareMathPassThrough{empheqrbrack}
+\DeclareMathPassThrough{empheqrceil}
+\DeclareMathPassThrough{empheqrfloor}
+\DeclareMathPassThrough{empheqrparen}
+\DeclareMathPassThrough{empheqrvert}
+\DeclareMathPassThrough{empheqr}
 
 \TeXMLendPackage
 
