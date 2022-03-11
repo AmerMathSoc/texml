7981c0137e0317a5986abd2bb42d66afb07960b6
David M. Jones <dmj@ams.org>
2022-03-11 12:30:44 -0500

    Added license statement to all source files.

--- lib/perl/TeX/Node/MarkNode.pm
+++ lib/perl/TeX/Node/MarkNode.pm
@@ -1,5 +1,34 @@
 package TeX::Node::MarkNode;
 
+# Copyright (C) 2022 American Mathematical Society
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For more details see, https://github.com/AmerMathSoc/texml
+
+# This code is experimental and is provided completely without warranty
+# or without any promise of support.  However, it is under active
+# development and we welcome any comments you may have on it.
+
+# American Mathematical Society
+# Technical Support
+# Publications Technical Group
+# 201 Charles Street
+# Providence, RI 02904
+# USA
+# email: tech-support@ams.org
+
 use strict;
 use warnings;
 
