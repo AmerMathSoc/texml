bfac521a225cab094d0545545573a00e899ecb1b
David M. Jones <dmj@ams.org>
2022-05-07 13:38:44 -0400

    Save snapshot of some experimental primitives.

---
+++ lib/perl/TeX/Primitive/Extension/addRowCSSclass.pm
@@ -0,0 +1,63 @@
+package TeX::Primitive::Extension::addRowCSSclass;
+
+# Copyright (C) 2022 American Mathematical Society
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For more details see, https://github.com/AmerMathSoc/texml
+
+# This code is experimental and is provided completely without warranty
+# or without any promise of support.  However, it is under active
+# development and we welcome any comments you may have on it.
+
+# American Mathematical Society
+# Technical Support
+# Publications Technical Group
+# 201 Charles Street
+# Providence, RI 02904
+# USA
+# email: tech-support@ams.org
+
+use strict;
+use warnings;
+
+use base qw(TeX::Command::Executable);
+
+use TeX::Class;
+
+use TeX::Constants qw(:named_args);
+
+use TeX::Node::XmlClassNode qw(:constants);
+
+sub execute {
+    my $self = shift;
+
+    my $tex     = shift;
+    my $cur_tok = shift;
+
+    my $css_property   = $tex->read_undelimited_parameter(EXPANDED);
+    my $property_value = $tex->read_undelimited_parameter(EXPANDED);
+
+    my $css_class = $tex->find_css_class($css_property, $property_value);
+
+    my $align = $tex->get_cur_alignment();
+
+    $align->push_row_class($css_class);
+
+    return;
+}
+
+1;
+
+__END__
