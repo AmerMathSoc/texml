620403b923db6bf4aeaa9de15b5cdf2499921f06
David M. Jones <dmj@ams.org>
2024-01-24 19:56:22 -0500

    Preliminary support for MOSC, SPMJ, and SUGA class files.

---
+++ lib/perl/TeX/Interpreter/LaTeX/Class/mosc_l.pm
@@ -0,0 +1,70 @@
+package TeX::Interpreter::LaTeX::Class::mosc_l;
+
+# Copyright (C) 2022 American Mathematical Society
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For more details see, https://github.com/AmerMathSoc/texml
+
+# This code is experimental and is provided completely without warranty
+# or without any promise of support.  However, it is under active
+# development and we welcome any comments you may have on it.
+
+# American Mathematical Society
+# Technical Support
+# Publications Technical Group
+# 201 Charles Street
+# Providence, RI 02904
+# USA
+# email: tech-support@ams.org
+
+use strict;
+use warnings;
+
+sub install ( $ ) {
+    my $class = shift;
+
+    my $tex = shift;
+
+    $tex->class_load_notification();
+
+    $tex->read_package_data();
+
+    return;
+}
+
+1;
+
+__DATA__
+
+\ProvidesClass{mosc-l}
+
+\DeclareOption*{\PassOptionsToClass{\CurrentOption}{amsart}}
+
+\ProcessOptions\relax
+
+\LoadClass{amsart}[1996/10/24]
+
+\publinfo{mosc}{}{}
+
+% TBD: Russian title (Trudy Moskov. Matem. Obshch.)
+
+\def\AMS@publname{Trans. Moscow Math. Soc.}
+
+\def\AMS@eissn{1547-738X}
+\def\AMS@pissn{0077-1554}
+
+\endinput
+
+__END__
