0d1294f978063080254f2e73ebd494bd648671c9
David M. Jones <dmj@ams.org>
2024-03-11 10:30:32 -0400

    TeX::Interpreter::LaTeX::Package::amsrefs: Patched \parse@arXiv to handle arXiv categories.

--- lib/perl/TeX/Interpreter/LaTeX/Package/amsrefs.pm
+++ lib/perl/TeX/Interpreter/LaTeX/Package/amsrefs.pm
@@ -489,7 +489,11 @@ __DATA__
 \def\parse@arXiv#1 [#2]#3\@nnil{%
     \def\arXiv@number{#1}%
     \def\arXiv@category{#2}%
-    \def\arXiv@url{https://arxiv.org/abs/#1}%
+    \ifx\arXiv@category\@empty
+        \def\arXiv@url{https://arxiv.org/abs/#1}%
+    \else
+        \def\arXiv@url{https://arxiv.org/abs/\arXiv@category/#1}%
+    \fi
 }
 
 \providecommand{\arXiv}[1]{%
