7fa003349bfae7969faa93ac898e9b9fd0acff7b
David M. Jones <dmj@ams.org>
2024-07-29 10:45:37 -0400

    TeX::Interpreter::LaTeX::Package::stix2, Add some stix2 characters that aren't in unicode-math: \varsubsetneq, \varsupsetneq, \varsubsetneqq, \varsupsetneqq, \varkappa.

--- lib/perl/TeX/Interpreter/LaTeX/Package/stix2.pm
+++ lib/perl/TeX/Interpreter/LaTeX/Package/stix2.pm
@@ -1,6 +1,6 @@
 package TeX::Interpreter::LaTeX::Package::stix2;
 
-# Copyright (C) 2022 American Mathematical Society
+# Copyright (C) 2022, 2024 American Mathematical Society
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -57,6 +57,12 @@ __DATA__
 \ProvidesPackage{stix2}
 
 \RequirePackage{unicode-math}
+
+\DeclareMathPassThrough{varsubsetneq}
+\DeclareMathPassThrough{varsupsetneq}
+\DeclareMathPassThrough{varsubsetneqq}
+\DeclareMathPassThrough{varsupsetneqq}
+\DeclareMathPassThrough{varkappa}
 
 \endinput
 
