b839fa42ee5a159d0375d489ab1afe3848622c1b
David M. Jones <dmj@ams.org>
2024-08-14 14:10:15 -0400

    TeX::Interpreter::LaTeX::Class::notices: Load wrapfig.

--- lib/perl/TeX/Interpreter/LaTeX/Class/notices.pm
+++ lib/perl/TeX/Interpreter/LaTeX/Class/notices.pm
@@ -1,6 +1,6 @@
 package TeX::Interpreter::LaTeX::Class::notices;
 
-# Copyright (C) 2022 American Mathematical Society
+# Copyright (C) 2022, 2024 American Mathematical Society
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -67,6 +67,7 @@ __DATA__
 \RequirePackage{xspace}
 \RequirePackage{amsthm}
 \RequirePackage{subfig}
+\RequirePackage{wrapfig}
 \RequirePackage[table]{xcolor}
 
 \RequirePackage[lite,nobysame]{amsrefs}
