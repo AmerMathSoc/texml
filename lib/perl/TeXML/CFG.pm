2da7f64136d6c9d23332f948baf55b2cfc4dde09
David M. Jones <dmj@ams.org>
2025-08-16 18:31:52 -0400

    TeXML::CFG: Implemented better auto-detection of TEXML_ROOT, but also make it easier to override in test scripts.

--- lib/perl/TeXML/CFG.pm
+++ lib/perl/TeXML/CFG.pm
@@ -1,6 +1,8 @@
 package TeXML::CFG;
 
-# Copyright (C) 2022 American Mathematical Society
+use v5.26.0;
+
+# Copyright (C) 2022, 2025 American Mathematical Society
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -29,41 +31,76 @@ package TeXML::CFG;
 # USA
 # email: tech-support@ams.org
 
-use strict;
 use warnings;
 
-use version; our $VERSION = qv '2.0.0';
+use Config::IniFiles;
 
-use base qw(Config::IniFiles);
+use File::Spec::Functions qw(catfile);
 
 use FindBin;
 
-(my $TEXML_ROOT = $FindBin::RealBin) =~ s{/bin$}{};
+use TeX::Class;
+
+my %root_of     :ATTR(:name<root>);
+
+my %cfg_dir_of  :ATTR(:name<cfg_dir>);
+my %cfg_file_of :ATTR(:name<cfg_file>);
+
+my %config_of :ATTR(:name<config>);
+
+######################################################################
+##                                                                  ##
+##                           CONSTRUCTOR                            ##
+##                                                                  ##
+######################################################################
+
+sub BUILD :RESTRICTED { }
+
+sub START {
+    my ($self, $ident, $arg_ref) = @_;
+
+    if (! defined $self->get_root()) {
+        my $root = $FindBin::RealBin =~ s{/texml\b.*$}{/texml}r;
+
+        $self->set_root($root);
+    }
+
+    if (! defined $self->get_cfg_dir()) {
+        my $cfg_dir = catfile($self->get_root(), 'cfg');
+
+        $self->set_cfg_dir($cfg_dir);
+    }
+
+    if (! defined $self->get_cfg_file()) {
+        my $program_name = $0 =~ s{^.*/}{}r;
+
+        my $cfg_file = "$program_name.cfg";
+
+        $self->set_cfg_file($cfg_file);
+    }
+
+    return;
+}
 
 GET_CFG: {
     my $CFG;
 
     sub get_cfg {
-        my $class = shift;
-        my $arg_ref = shift; # || {};
+        my $class   = shift;
+        my $arg_ref = shift || {};
 
         return $CFG if defined $CFG;
 
-        $CFG = __PACKAGE__->new(-default => 'DEFAULTS',
-                                -allowcontinue => 1);
+        my $config = Config::IniFiles->new(-default => 'DEFAULTS',
+                                           -allowcontinue => 1);
 
-        my $cfg_file = $arg_ref->{cfg_file};;
+        $CFG = __PACKAGE__->new({ $arg_ref->%*, config => $config });
 
-        if (! defined $cfg_file) {
-            (my $program_name = $0) =~ s{^.*/}{};
-
-            $cfg_file = "$program_name.cfg";
-        }
-
-        $cfg_file = "$FindBin::RealBin/../cfg/$cfg_file";
+        my $cfg_file = catfile($CFG->get_cfg_dir(), $CFG->get_cfg_file());
 
         if (-e $cfg_file) {
             $CFG->SetFileName($cfg_file);
+
             $CFG->ReadConfig();
         }
 
@@ -77,11 +114,33 @@ GET_CFG: {
 sub val {
     my $self = shift;
 
-    my $val = $self->SUPER::val(@_);
+    my $val = $self->get_config()->val(@_);
 
-    $val =~ s{\$TEXML_ROOT\b}{$TEXML_ROOT} if defined $val;
+    if (defined (my $root = $self->get_root())) {
+        $val =~ s{\$TEXML_ROOT\b}{$root} if defined $val;
+    }
 
     return $val;
+}
+
+######################################################################
+##                                                                  ##
+##                         AUTOMETHOD MAGIC                         ##
+##                                                                  ##
+######################################################################
+
+sub AUTOMETHOD {
+    my ($self, $ident, @args) = @_;
+
+    my $subname = $_;   # Requested subroutine name is passed via $_
+
+    my $config = $self->get_config();
+
+    if ($config->can($subname)) {
+        return sub() { return $config->$subname(@args) };
+    }
+
+    return;
 }
 
 1;
