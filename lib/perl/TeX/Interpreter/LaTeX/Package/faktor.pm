1e0f97fb9fa3a0d3a4712148183789c5beeb8feb
David M. Jones <dmj@ams.org>
2025-09-17 20:59:16 -0400

    TeX::Interpreter::LaTeX::Package::faktor: Emulate \faktor until we can get something better.

---
+++ lib/perl/TeX/Interpreter/LaTeX/Package/faktor.pm
@@ -0,0 +1,63 @@
+package TeX::Interpreter::LaTeX::Package::faktor;
+
+use 5.26.0;
+
+# Copyright (C) 2025 American Mathematical Society
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For more details see, https://github.com/AmerMathSoc/texml
+
+# This code is experimental and is provided completely without warranty
+# or without any promise of support.  However, it is under active
+# development and we welcome any comments you may have on it.
+
+# American Mathematical Society
+# Technical Support
+# Publications Technical Group
+# 201 Charles Street
+# Providence, RI 02904
+# USA
+# email: tech-support@ams.org
+
+use warnings;
+
+sub install  {
+    my $class = shift;
+
+    my $tex = shift;
+
+    $tex->package_load_notification();
+
+    $tex->read_package_data();
+
+    return;
+}
+
+1;
+
+__DATA__
+
+\ProvidesPackage{faktor}
+
+% \LoadRawMacros
+
+% This isn't quite right, but it's probably the best we can do without
+% help from MathJax.
+
+\def\faktor#1#2{{}^{\textstyle#1}\mskip-2mu/\mskip-2mu_{\textstyle#2}}
+
+\endinput
+
+__END__
