d3254a697b3dfc18617369506e92564638de5403
David M. Jones <dmj@ams.org>
2025-11-07 11:35:19 -0500

    Added class files for COLL, CONM, HMATH, MBK, PCMS, PSAPM, PSPUM, and SURV.

---
+++ lib/perl/TeX/Interpreter/LaTeX/Class/hmath_l.pm
@@ -0,0 +1,67 @@
+package TeX::Interpreter::LaTeX::Class::hmath_l;
+
+use 5.26.0;
+
+# Copyright (C) 2025 American Mathematical Society
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For more details see, https://github.com/AmerMathSoc/texml
+
+# This code is experimental and is provided completely without warranty
+# or without any promise of support.  However, it is under active
+# development and we welcome any comments you may have on it.
+
+# American Mathematical Society
+# Technical Support
+# Publications Technical Group
+# 201 Charles Street
+# Providence, RI 02904
+# USA
+# email: tech-support@ams.org
+
+use warnings;
+
+sub install {
+    my $class = shift;
+
+    my $tex = shift;
+
+    $tex->class_load_notification();
+
+    $tex->read_package_data();
+
+    return;
+}
+
+1;
+
+__DATA__
+
+\ProvidesClass{hmath_l}
+
+\LoadClass{amsbook}
+
+\seriesinfo{hmath}{}{}
+
+\def\AMS@publname{History of Mathematics}
+
+\def\AMS@pissn{0899-2428}
+\def\AMS@eissn{2472-4785}
+
+\def\AMS@series@url{https://www.ams.org/publications/ebooks/hmath-mon}
+
+\endinput
+
+__END__
---
+++ lib/perl/TeX/Interpreter/LaTeX/Class/mbk_l.pm
@@ -0,0 +1,67 @@
+package TeX::Interpreter::LaTeX::Class::mbk_l;
+
+use 5.26.0;
+
+# Copyright (C) 2025 American Mathematical Society
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# For more details see, https://github.com/AmerMathSoc/texml
+
+# This code is experimental and is provided completely without warranty
+# or without any promise of support.  However, it is under active
+# development and we welcome any comments you may have on it.
+
+# American Mathematical Society
+# Technical Support
+# Publications Technical Group
+# 201 Charles Street
+# Providence, RI 02904
+# USA
+# email: tech-support@ams.org
+
+use warnings;
+
+sub install {
+    my $class = shift;
+
+    my $tex = shift;
+
+    $tex->class_load_notification();
+
+    $tex->read_package_data();
+
+    return;
+}
+
+1;
+
+__DATA__
+
+\ProvidesClass{mbk_l}
+
+\LoadClass{amsbook}
+
+\seriesinfo{mbk}{}{}
+
+\def\AMS@publname{AMS Non-Series Monographs}
+
+\def\AMS@pissn{2693-0439} % ??
+% \def\AMS@eissn{}
+
+\def\AMS@series@url{https://www.ams.org/publications/ebooks/mbk-mon}
+
+\endinput
+
+__END__
