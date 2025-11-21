5f14b9d76e770cba7d6969192b7b6e6be67c7d7b
David M. Jones <dmj@ams.org>
2025-11-21 10:22:40 -0500

    TeX::Interpreter::LaTeX::Package::epigraph: Add check for empty attribution.

--- lib/perl/TeX/Interpreter/LaTeX/Package/epigraph.pm
+++ lib/perl/TeX/Interpreter/LaTeX/Package/epigraph.pm
@@ -1,6 +1,8 @@
 package TeX::Interpreter::LaTeX::Package::epigraph;
 
-# Copyright (C) 2022 American Mathematical Society
+use v5.26.0;
+
+# Copyright (C) 2022, 2025 American Mathematical Society
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -29,10 +31,9 @@ package TeX::Interpreter::LaTeX::Package::epigraph;
 # USA
 # email: tech-support@ams.org
 
-use strict;
 use warnings;
 
-sub install ( $ ) {
+sub install {
     my $class = shift;
 
     my $tex = shift;
@@ -57,7 +58,9 @@ __DATA__
     \startXMLelement{disp-quote}%
     \setXMLattribute{content-type}{epigraph}%
     #1\par
-    \thisxmlpartag{attrib}#2\par
+    \ifnum\strcmp{#2}{}=0\else
+        \thisxmlpartag{attrib}#2\par
+    \fi
     \endXMLelement{disp-quote}%
     \par
 }
