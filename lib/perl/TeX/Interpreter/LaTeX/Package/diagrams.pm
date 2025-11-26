8d50d9f5cd7b762aa2bd6f89f72b0e6786329173
David M. Jones <dmj@ams.org>
2025-11-26 11:58:36 -0500

    TeX::Interpreter::LaTeX::Package::diagrams: Corrected \ProvidesPackage

--- lib/perl/TeX/Interpreter/LaTeX/Package/diagrams.pm
+++ lib/perl/TeX/Interpreter/LaTeX/Package/diagrams.pm
@@ -1,6 +1,8 @@
 package TeX::Interpreter::LaTeX::Package::diagrams;
 
-# Copyright (C) 2022 American Mathematical Society
+use v5.26.0;
+
+# Copyright (C) 2022, 2025 American Mathematical Society
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -29,10 +31,9 @@ package TeX::Interpreter::LaTeX::Package::diagrams;
 # USA
 # email: tech-support@ams.org
 
-use strict;
 use warnings;
 
-sub install ( $ ) {
+sub install {
     my $class = shift;
 
     my $tex = shift;
@@ -54,7 +55,7 @@ sub install ( $ ) {
 
 __DATA__
 
-\ProvidesPackage{diagrams.pm}
+\ProvidesPackage{diagrams}
 
 \DeclareSVGEnvironment{diagram}
 
