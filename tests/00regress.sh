c0ed47290aab618ca3a1a43a4875065779d44232
David M. Jones <dmj@ams.org>
2026-02-19 17:51:14 -0500

    00regress.sh: Check log files for errors.  Also strip compound extensions from the input filenames.

--- tests/00regress.sh
+++ tests/00regress.sh
@@ -4,6 +4,7 @@ texml="${0%%/*}/../bin/texml"
 
 declare -i numtests=0
 declare -i numfailed=0
+declare -i numwarnings=0
 declare -i numsucceeded=0
 declare -i numskipped=0
 
@@ -17,9 +18,7 @@ for test_file in $test_files
 do
     numtests=numtests+1
 
-    test_name=${test_file%.*}
-    # test_name=${test_name%-ref}
-    # test_name=${test_name%-diff}
+    test_name=${test_file%%.*}
 
     echo -n $test_name...
 
@@ -28,13 +27,30 @@ do
 
         if $texml $test_name.tex > /dev/null; then
             echo "texml succeeded"
+
+            echo -n "    checking log file..."
+
+            cmd='egrep \^\! $test_name.log | fgrep -v "Deleting empty paragraph"'
+
+            if eval $cmd > /dev/null; then
+                echo "errors found:"
+
+                awk '/^!/ && ! /empty paragraph/ { print "       ", $0 } ' $test_name.log
+
+                numwarnings=numwarnings+1
+            else
+                echo "clean!"
+            fi
+
             echo -n "    checking XML output..."
+
             if diff -q $test_name.xml $test_name.xml.ref; then
                 echo "clean!"
             else
                 echo "FAILED (XML files differ)"
                 success=0
             fi
+
             if [ -e $test_name.css.ref ]; then
                 echo -n "    checking CSS output..."
                 if diff -q $test_name.css $test_name.css.ref; then
@@ -64,6 +80,7 @@ done
 
 echo "tests:      $numtests"
 echo "successful: $numsucceeded"
+echo "warnings:   $numwarnings"
 echo "skipped:    $numskipped"
 echo "failed:     $numfailed"
 
